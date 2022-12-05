fca9c3b5b8c5f623912213c7a281dfdbc2d68d19
DarioMarret <javier_dario_marret@hotmail.com>
2022-12-05 13:07:13 -0500

    resr

--- model/edificio.py
+++ model/edificio.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Table, Column
-from sqlalchemy import Integer, String, DateTime, ForeignKey, BLOB
+from sqlalchemy import Integer, String, DateTime, ForeignKey, BLOB, Text
 from database.db import meta, db
 
 Edicifios = Table("edificio", meta, 
@@ -9,7 +9,7 @@ Edicifios = Table("edificio", meta,
     Column("ctaReferencia", String(255)), 
     Column("nombreEdificio", String(255)), 
     Column("referencia", String(255)), 
-    Column("adjunto", BLOB(4294967295)), 
+    Column("adjunto", Text(4294967295)), 
     Column("data_creatd", DateTime), 
     Column("data_update", DateTime)
 )
