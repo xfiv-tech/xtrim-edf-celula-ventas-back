5c45a20e91e5ac3326345c02db17d5c7c6b42c59
DarioMarret <javier_dario_marret@hotmail.com>
2023-02-28 19:29:57 -0500

    2023-02-28_19:29:57

--- build.sh
+++ build.sh
@@ -6,13 +6,12 @@ git push
 
 export SHORT_COMMIT=$(git log -1 --pretty="%H" | cut -b -8)
 export DOCKER_IMAGE_VERSION="dev_${SHORT_COMMIT}"
-# export DOCKER_IMAGE_VERSION="dev_12356"
+
+pip freeze > requirements.txt
+
 
 docker build -t intelnexoec/xtrim-api-edificio:${DOCKER_IMAGE_VERSION} -f Dockerfile .
 docker tag intelnexoec/xtrim-api-edificio:${DOCKER_IMAGE_VERSION} intelnexoec/xtrim-api-edificio:latest
 docker push intelnexoec/xtrim-api-edificio:${DOCKER_IMAGE_VERSION}
 docker push intelnexoec/xtrim-api-edificio:latest
 echo "tag: ${DOCKER_IMAGE_VERSION}"
-
-# docker build -t xtrim-api-informacion -f Dockerfile .
-# docker run -p 3003:3003 xtrim-api-informacion
