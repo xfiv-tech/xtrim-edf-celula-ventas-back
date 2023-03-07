0b526f22f846b9837729d21a600ba5c9c60a6887
DarioMarret <javier_dario_marret@hotmail.com>
2023-03-07 12:01:41 -0500

    2023-03-07_12:01:41

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ git add .
 git commit -m "$(date +%Y-%m-%d_%H:%M:%S)"
 
 git push
-docker login -u "intelnexoec" -p "DiegoySophie2O22" docker.io
+docker login -u "intelnexoec" -p "dckr_pat_EY2hedAJR91hc9Vn9Hx6-qVpgg0"
 export SHORT_COMMIT=$(git log -1 --pretty="%H" | cut -b -8)
 export DOCKER_IMAGE_VERSION="dev_${SHORT_COMMIT}"
 
