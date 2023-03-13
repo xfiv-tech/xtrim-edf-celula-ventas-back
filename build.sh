05931e0fca301fa9d53be6b1c95c0453ba95b055
DarioMarret <javier_dario_marret@hotmail.com>
2023-03-13 18:00:25 -0500

    2023-03-13_18:00:25

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
+# git pull origin feat/addCatalogsAndEmployees
+
 git add .
 
 git commit -m "$(date +%Y-%m-%d_%H:%M:%S)"
 
 git push
+
 docker login -u "intelnexoec" -p "dckr_pat_EY2hedAJR91hc9Vn9Hx6-qVpgg0"
+
 export SHORT_COMMIT=$(git log -1 --pretty="%H" | cut -b -8)
 export DOCKER_IMAGE_VERSION="dev_${SHORT_COMMIT}"
 
