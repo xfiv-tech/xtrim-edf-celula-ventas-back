f4de94d6fa0cffe3e99087ae374010e6790be071
DarioMarret <javier_dario_marret@hotmail.com>
2023-03-18 22:45:29 -0500

    2023-03-18_22:45:29

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,9 @@ RUN pip install --no-cache-dir --upgrade -r /xtrim/requirements.txt
 
 RUN ls -la
 
+LABEL maintainer="Dario Javier Marret medranda <javier_dario_marret@hotmail.com>" \
+      version="1.0" \
+      description="Xtrim API Celulas de ventas"
 # ENV user=root password=Marret123456+-* host=146.190.208.100:3306 database=database
 
 CMD python -m uvicorn index:app --host 0.0.0.0 --port 3003
--- function/ftp.py
+++ function/ftp.py
@@ -1,31 +1,31 @@
-import ftplib
+# import ftplib
 
-def ftp_connect(host, user, passwd):
-    ftp = ftplib.FTP(host)
-    ftp.login(user, passwd)
-    return ftp
-
-
-def ftp_list(ftp, path):
-    return ftp.nlst(path)
+# def ftp_connect(host, user, passwd):
+#     ftp = ftplib.FTP(host)
+#     ftp.login(user, passwd)
+#     return ftp
 
 
-def ftp_mkdir(ftp, path):
-    ftp.mkd(path)
+# def ftp_list(ftp, path):
+#     return ftp.nlst(path)
 
 
-def ftp_rmdir(ftp, path):
-    ftp.rmd(path)
-
-def ftp_upload(ftp, file, path):
-    ftp.storbinary('STOR ' + path, file)
+# def ftp_mkdir(ftp, path):
+#     ftp.mkd(path)
 
 
-def ftp_download(ftp, path, file):
-    ftp.retrbinary('RETR ' + path, file.write)
+# def ftp_rmdir(ftp, path):
+#     ftp.rmd(path)
+
+# def ftp_upload(ftp, file, path):
+#     ftp.storbinary('STOR ' + path, file)
 
 
-def ftp_close(ftp):
-    ftp.quit()
+# def ftp_download(ftp, path, file):
+#     ftp.retrbinary('RETR ' + path, file.write)
+
+
+# def ftp_close(ftp):
+#     ftp.quit()
 
 
