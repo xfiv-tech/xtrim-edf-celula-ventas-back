6b05856f0503b1c45cd5bdb07e642d8fc53e217c
DarioMarret <javier_dario_marret@hotmail.com>
2023-04-03 10:36:25 -0500

    2023-04-03_10:36:25

--- routes/login.py
+++ routes/login.py
@@ -74,9 +74,10 @@ async def ValidacionLogin(datos: Login):
 @login.post("/login_celula", tags=["login"])
 async def ValidacionLoginCelula(datos: Login):
     try:
+        print("datos",datos)
         query = RegistroAdministrador.select().where(RegistroAdministrador.c.email == datos.email)
         user = db.execute(query).first()
-        decr_data = checkPassword(datos.password, user["password"])
+        decr_data = checkPassword(datos.password, user.password)
         menu = Menus.select().where(Menus.c.id_roles == user.id_roles)
         menu = db.execute(menu).fetchall()
         info_menu = []
