6fd71a2ceb09a026b821216be7c586792bb1e802
DarioMarret <javier_dario_marret@hotmail.com>
2023-04-04 20:44:32 -0500

    2023-04-04_20:44:32 habilitando el endpoit de codigo de vendedor

--- function/CodigoVendedor.py
+++ function/CodigoVendedor.py
@@ -1,17 +1,16 @@
 import requests
 import uuid
-# session = requests.Session()
-# session.verify = False
+import json
 
 async def LoginCodigo():
     try:
-
-        login = requests.post("https://apix.grupotvcable.com/rest/token-api/v1.0/generate", data={
+        payload = json.dumps({
             "channel": "Celula_Ventas",
             "key": "YXBpbS1jbGllbnQ6N2M4ZmEyMDgtMmZjZC00YjhiLTk1ZjktYTIwZWJkODliYmRi",
             "realm": "realm-ecommerce",
             "type": "Basic"
         })
+        login = requests.post("https://apix.grupotvcable.com/rest/token-api/v1.0/generate", data=payload, headers={'Content-Type': 'application/json'})
         print("Login",login.json())
         response = login.json()
         return response["data"]["token"]
@@ -24,14 +23,13 @@ async def ConsultarVendedor(codigo):
     try:
         print("externalTransactionId",str(uuid.uuid1()))
         response = await LoginCodigo()
-        code = requests.post("https://apix.grupotvcable.com/rest/salesperson-api/v1.0/queryvendor", data={
+        payload = json.dumps({
             "channel": "TYTAN",
             "idVendor": codigo,
             "application": "Marketplace",
             "externalTransactionId": str(uuid.uuid1()),
-        }, headers={
-            'Authorization': 'Bearer ' + response,
         })
+        code = requests.post("https://apix.grupotvcable.com/rest/salesperson-api/v1.0/queryvendor", data=payload, headers={'Authorization': 'Bearer ' + response, 'Content-Type': 'application/json'})
         print(code)
         vendedor = code.json()
         if(vendedor["code"] == 400):
