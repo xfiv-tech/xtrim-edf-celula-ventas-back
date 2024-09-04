80993937138f44d36db3ff2d5be9e79b35dba299
geampierejaramillo <jeanpiere.virgilio@gmail.com>
2024-09-03 19:04:33 -0500

    2024-09-03_19:04:32 Se refacturizo para que se acepte id_gerenre_regional valla con valor null y ya no sea obligatorio

--- routes/login.py
+++ routes/login.py
@@ -10,7 +10,6 @@ from cryptography.fernet import Fernet
 from function.encrytPassword import checkPassword
 
 
-
 key = Fernet.generate_key()
 f = Fernet(key)
 
@@ -27,16 +26,18 @@ async def ValidacionLogin(datos: Login):
         menu = db.execute(menu).fetchall()
         info_menu = []
         for i in menu:
-            print("i_menus",i.menu)
+            print("i_menus", i.menu)
             exc = Submenus.select().where(Submenus.c.id_menus == i.id_menus)
             sub = db.execute(exc).fetchall()
-            info_menu.append({
-                "id_menus": i.id_menus,
-                "menu": i.menu,
-                "path": i.path,
-                "icon": i.icon,
-                "submenu": sub
-            })
+            info_menu.append(
+                {
+                    "id_menus": i.id_menus,
+                    "menu": i.menu,
+                    "path": i.path,
+                    "icon": i.icon,
+                    "submenu": sub,
+                }
+            )
         if len(user) > 0:
             if decr_data == True:
                 true_user = {
@@ -50,78 +51,69 @@ async def ValidacionLogin(datos: Login):
                     "code": "0",
                     "token": write_token(dict(true_user)),
                     "menu": info_menu,
-                    "message": "Login correcto"
+                    "message": "Login correcto",
                 }
             else:
-                return {
-                    "code": "-1",
-                    "data": [],
-                    "message": "Contraseña incorrecta"
-                }
+                return {"code": "-1", "data": [], "message": "Contraseña incorrecta"}
         else:
-            return {
-                "code": "-1",
-                "data": [],
-                "message": "Usuario no existe"
-            }
+            return {"code": "-1", "data": [], "message": "Usuario no existe"}
     except Exception as e:
-        raise HTTPException(status_code=400, detail={
-            "code": "-1",
-            "data": str(e.args)
-        })
-    
+        raise HTTPException(status_code=400, detail={"code": "-1", "data": str(e.args)})
+
 
 @login.post("/login_celula", tags=["login"])
 async def ValidacionLoginCelula(datos: Login):
     try:
-        print("datos",datos)
-        query = RegistroAdministrador.select().where(RegistroAdministrador.c.email == datos.email)
+        query = RegistroAdministrador.select().where(
+            RegistroAdministrador.c.email == datos.email
+        )
         user = db.execute(query).first()
-        print("user",user)
+
+        if user is None:
+            return {"code": "-1", "data": [], "message": "Usuario no existe"}
+
         decr_data = checkPassword(datos.password, user.password)
-        menu = Menus.select().where(Menus.c.id_roles == user.id_roles)
-        menu = db.execute(menu).fetchall()
+        if not decr_data:
+            return {"code": "-1", "data": [], "message": "Contraseña incorrecta"}
+
+        menu_query = Menus.select().where(Menus.c.id_roles == user.id_roles)
+        menu = db.execute(menu_query).fetchall()
+
         info_menu = []
         for i in menu:
-            print("i_menus",i.menu)
             exc = Submenus.select().where(Submenus.c.id_menus == i.id_menus)
             sub = db.execute(exc).fetchall()
-            info_menu.append({
-                "id_menus": i.id_menus,
-                "menu": i.menu,
-                "path": i.path,
-                "icon": i.icon,
-                "submenu": sub
-            })
-        if len(user) > 0:
-            if decr_data == True:
-                true_user = {
-                    "id": user.id_administrador,
-                    "id_rol": user.id_roles,
-                    "email": user.email,
-                    "usuario": user.nombre_administrador,
-                    "perfil": user.perfil,
+
+            # Convert submenus to a serializable format
+            submenus_data = [
+                {"id_submenu": s.id_submenu, "submenu": s.submenu, "path": s.path}
+                for s in sub
+            ]
+
+            info_menu.append(
+                {
+                    "id_menus": i.id_menus,
+                    "menu": i.menu,
+                    "path": i.path,
+                    "icon": i.icon,
+                    "submenu": submenus_data,  # Use the serialized submenus
                 }
-                return {
-                    "code": "0",
-                    "token": write_token(dict(true_user)),
-                    "menu": info_menu,
-                    "message": "Login correcto"
-                }
-            else:
-                return {
-                    "code": "-1",
-                    "data": [],
-                    "message": "Contraseña incorrecta"
-                }
-        else:
-            return {
-                "code": "-1",
-                "data": [],
-                "message": "Usuario no existe"
-            }
+            )
+
+        true_user = {
+            "id": user.id_administrador,
+            "id_rol": user.id_roles,
+            "email": user.email,
+            "usuario": user.nombre_administrador,
+            "perfil": user.perfil,
+        }
+
+        return {
+            "code": "0",
+            "token": write_token(true_user),
+            "menu": info_menu,
+            "message": "Login correcto",
+        }
+
     except Exception as e:
-        raise HTTPException(status_code=400, detail={
-            "code": "-1",
-            "data": str(e.args)
-        })
+        raise HTTPException(status_code=400, detail={"code": "-1", "data": str(e.args)})
