a4c92a115d49eaf97805a1e3b9ea354aa87a16db
geampierejaramillo <jeanpiere.virgilio@gmail.com>
2024-09-15 12:30:14 -0500

    2024-09-15_12:30:14 Se refacturizo para que se acepte id_gerenre_regional valla con valor null y ya no sea obligatorio

--- app.py
+++ app.py
@@ -49,7 +49,9 @@ scheduler = AsyncIOScheduler()
 async def startup():
     print("Starting up...")
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(tarea_programada, CronTrigger(hour=23), id="tarea_programada")
+    scheduler.add_job(
+        tarea_programada, CronTrigger(hour=12, minute=35), id="tarea_programada"
+    )
     scheduler.start()
 
 
